9fa5541d0c74fbed396e3ad8b58d63d63a4a354c
振宇 沈 <876619304@qq.com>
2019-12-05 00:25:51 +0800

    添加

---
+++ WindowsForms/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsForms.Model;
+
+namespace WindowsForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+
+        }
+
+        private void queryDate_Click(object sender, EventArgs e)
+        {
+            using (DataClasses1DataContext dc = new DataClasses1DataContext())
+            {
+                var result = from a in dc.category
+                             select a;
+                foreach(var c in result)
+                {
+                    Debug.WriteLine("country category_name:" + c.category_name);
+                    Debug.WriteLine("parent_id:" + c.parent_id);
+                    Debug.WriteLine("is_leaf:" + c.is_leaf);
+                    Debug.WriteLine("id:" + c.id);
+                }
+                
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+    }
+}
---
+++ WindowsForms/Model/Category.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsForms.Model
+{
+    class Category
+    {
+        public long Id { get; set; }
+        public int  IsLeaf {get; set; }
+        public string CategoryName { get; set; }
+        public long ParentId { get; set; }
+    }
+}
