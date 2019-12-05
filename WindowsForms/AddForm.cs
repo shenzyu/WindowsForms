bb92c59db914c7df47ca2a6d33faa376dab56240
振宇 沈 <876619304@qq.com>
2019-12-05 23:35:36 +0800

    12.5

---
+++ WindowsForms/AddForm.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace WindowsForms
+{
+    public partial class AddForm : Form
+    {
+        public AddForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
